94011d8d47d6a91df68182adc4e3adc1c0a76f67
tafrij <mamunamri97@gmail.com>
2018-12-06 08:55:42 +0700

    git push origin master

--- koperasi/modul/lap-pinjaman/koneksi.php
+++
@@ -1,6 +0,0 @@
-<?php
-$server = "localhost";
-$username = "root";
-$password = "";
-$database = "koperasi_pds";
-$konek = new mysqli ($server, $username, $password, $database) or die ("Gagal konek ke server MySQL" .mysql_error());
--- koperasi/readme.txt
+++
@@ -1,4 +0,0 @@
-Username : admin
-password : admin
-
-silahkan follow/like/subscribe blog kami untuk mendapatkan informasi aplikasi - aplikasi terbaru 
