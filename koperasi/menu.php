44ba561c0734828a38c7dfcdc28860714e4da636
tafrij <mamunamri97@gmail.com>
2018-12-06 09:00:31 +0700

    git add *

---
+++ koperasi/menu.php
@@ -0,0 +1,27 @@
+<ul class="sf-menu">
+<li><a href="?module=home">Home</a></li>
+<li>
+<a href="#">Master</a>
+<ul>
+<li><a href="?module=jenis">Jenis Simpanan</a></li>
+<li><a href="?module=anggota">Anggota</a></li>
+</ul>
+</li>
+<li>
+<a href="#">Transaksi</a>
+<ul>
+<li><a href="?module=simpanan">Simpanan</a></li>
+<li><a href="?module=pinjaman">Pinjaman</a></li>
+<li><a href="?module=bayar">Bayar Pinjaman</a></li>
+</ul>
+</li>
+<li>
+<a href="#">Laporan</a>
+<ul>
+<li><a href="modul\lap-anggota">Anggota</a></li>
+<li><a href="modul\lap-simpanan">Simpanan</a></li>
+<li><a href="modul\lap-pinjaman">Pinjaman</a></li>
+</ul>
+</li>
+<li><a href="logout.php">Keluar</a></li>
+</ul>
